37610ef505dde118bf0160e96a2aad522486084f
safauludogan <suludogan95@gmail.com>
2024-06-23 16:59:48 +0300

    Modules added and packages added to pubspec. settings.json configured

---
+++ lib/feature/home/view/mixin/home_view_mixin.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_weefit/feature/home/view/home_view.dart';
+import 'package:widgets/widgets.dart';
+
+mixin HomeViewMixin on State<HomeView> {
+  @override
+  void initState() {
+    super.initState();
+
+    final safa = Safa();
+  }
+}
