ba3f1e1c29c7331cfb7022a676301de090cd57ad
safauludogan <suludogan95@gmail.com>
2024-06-23 20:59:52 +0300

    project localization added

---
+++ lib/product/init/language/locale_keys.g.dart
@@ -0,0 +1,14 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const hello_title = 'hello.title';
+  static const hello = 'hello';
+  static const general_button_save = 'general.button.save';
+  static const general_button = 'general.button';
+  static const general_dialog_version_title = 'general.dialog.version.title';
+  static const general_dialog_version_content = 'general.dialog.version.content';
+  static const general_dialog_version = 'general.dialog.version';
+  static const general_dialog = 'general.dialog';
+  static const general = 'general';
+
+}
