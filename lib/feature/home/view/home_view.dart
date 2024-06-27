923587ffd92159221cce43319e2b232e0b9588f4
safauludogan <suludogan95@gmail.com>
2024-06-27 15:42:25 +0300

    auto route added

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -1,16 +1,19 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_weefit/feature/home/view/mixin/home_view_mixin.dart';
 import 'package:flutter_weefit/product/init/config/app_environment.dart';
 import 'package:flutter_weefit/product/init/language/locale_keys.g.dart';
 import 'package:flutter_weefit/product/init/product_localization.dart';
+import 'package:flutter_weefit/product/navigation/app_router.dart';
 import 'package:flutter_weefit/product/utility/constants/enums/locales.dart';
 import 'package:gen/gen.dart';
 import 'package:kartal/kartal.dart';
 
 part 'widget/home_app_bar.dart';
 
-class HomeView extends StatefulWidget {
+@RoutePage()
+final class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
@@ -29,11 +32,13 @@ class _HomeViewState extends State<HomeView> with HomeViewMixin {
             Assets.icons.icCheck.svg(
               package: 'gen',
             ),
-            Assets.images.imgBottle.image(
-              package: 'gen'
-            ),
+            Assets.images.imgBottle.image(package: 'gen'),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                final response = context.router.push<bool?>(
+                  HomeDetailRoute(id: '1'),
+                );
+              },
               child: Text(
                 AppEnvironmentItems.baseUrl.value,
                 style: context.general.textTheme.bodyLarge,
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_weefit/feature/home/view/home_view.dart';
 import 'package:flutter_weefit/product/init/application_initialize.dart';
 import 'package:flutter_weefit/product/init/product_localization.dart';
 import 'package:flutter_weefit/product/init/theme/custom_dark_theme.dart';
 import 'package:flutter_weefit/product/init/theme/custom_light_theme.dart';
+import 'package:flutter_weefit/product/navigation/app_router.dart';
 
 Future<void> main() async {
   await ApplicationInitialize().make();
@@ -15,16 +15,18 @@ Future<void> main() async {
 class _MyApp extends StatelessWidget {
   const _MyApp({super.key});
 
+  static final _appRouter = AppRouter();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _appRouter.config(),
       theme: CustomLightTheme().themeData,
       darkTheme: CustomDarkTheme().themeData,
       debugShowCheckedModeBanner: false,
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
-      home: const HomeView(),
     );
   }
 }
