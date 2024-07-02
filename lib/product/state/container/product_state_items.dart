c2eae5cd9e390a0c8c4504b09db377f7ae7200de
safauludogan <suludogan95@gmail.com>
2024-07-02 21:07:27 +0300

    Cache manager with hive #16

--- lib/product/state/container/product_state_items.dart
+++ lib/product/state/container/product_state_items.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_weefit/product/cache/product_cache.dart';
 import 'package:flutter_weefit/product/service/manager/product_service_manager.dart';
 import 'package:flutter_weefit/product/state/container/product_state_container.dart';
 import 'package:flutter_weefit/product/state/view_model/product_view_model.dart';
@@ -10,4 +11,7 @@ final class ProductStateItems {
 
   static ProductViewModel get productViewModel =>
       ProductContainer.read<ProductViewModel>();
+
+  static ProductCache get productCache =>
+      ProductContainer.read<ProductCache>();
 }
